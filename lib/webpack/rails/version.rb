f30d66dd088de8ca466a7b464084ee9ffee01613
Dave Copeland <davetron5000@gmail.com>
2017-02-25 09:22:23 -0500

    buump version

--- lib/webpack/rails/version.rb
+++ lib/webpack/rails/version.rb
@@ -1,6 +1,6 @@
 module Webpack
   # :nodoc:
   module Rails
-    VERSION = "2.2.1" # matching the version of webpack
+    VERSION = "2.2.1.1" # matching the version of webpack, fourth patch is for us
   end
 end
